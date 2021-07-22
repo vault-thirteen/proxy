3ece78d3e2fe3a9bcd9e3d49563334a3ca60aa20
Vault 13 <54213347+vault-thirteen@users.noreply.github.com>
2021-07-22 23:16:47 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module test/ReverseProxy
+
+go 1.16
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/rs/zerolog v1.23.0
+)
---
+++ proxy.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"io"
+	"net"
+	"net/http"
+	"time"
+)
+
+func main() {
+	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+
+	server := http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: http.HandlerFunc(handler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	t1 := time.Now()
+
+	switch req.Method {
+	case http.MethodConnect:
+		processHttpsRequest(w, req)
+	default:
+		processHttpRequest(w, req)
+	}
+
+	log.Debug().Msgf("time taken to serve '%v' is %v", req.URL.String(), time.Since(t1).String())
+}
+
+func processHttpRequest(w http.ResponseWriter, req *http.Request) {
+	log.Debug().Msgf("http request to '%s'", req.URL.String())
+
+	req.RequestURI = ""
+	setClientHeaders(req.Header)
+
+	client := &http.Client{}
+
+	var targetResponse *http.Response
+	var err error
+	targetResponse, err = client.Do(req)
+	if err != nil {
+		http.Error(w, "client.do error", http.StatusInternalServerError)
+		log.Error().Err(err).Msg("")
+		return
+	}
+
+	setTargetHeaders(targetResponse.Header, w.Header())
+
+	w.WriteHeader(targetResponse.StatusCode)
+
+	// Copy body to ResponseWriter.
+	_, err = io.Copy(w, targetResponse.Body)
+	if err != nil {
+		log.Error().Err(err).Msg("")
+		return
+	}
+}
+
+func processHttpsRequest(w http.ResponseWriter, req *http.Request) {
+	log.Debug().Msgf("https request to '%s'", req.URL.String())
+
+	// Establish a TCP connection with target.
+	targetConn, err := net.Dial("tcp", req.URL.Host)
+	if err != nil {
+		http.Error(w, "net.dial error", http.StatusInternalServerError)
+		log.Error().Err(err).Msg("")
+		return
+	}
+
+	defer func() {
+		derr := targetConn.Close()
+		if derr != nil {
+			log.Error().Err(derr).Msg("")
+			return
+		}
+	}()
+
+	// Hijack the client's connection.
+	hjk, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "hijacking is not supported", http.StatusInternalServerError)
+		log.Error().Msg("hijacking is not supported")
+		return
+	}
+
+	clientConn, _, err := hjk.Hijack()
+	if err != nil {
+		http.Error(w, "hijack error", http.StatusInternalServerError)
+		log.Error().Err(err).Msg("")
+		return
+	}
+
+	defer func() {
+		derr := clientConn.Close()
+		if derr != nil {
+			log.Error().Err(derr).Msg("")
+			return
+		}
+	}()
+
+	// Accept the HTTPS upgrade.
+	_, err = clientConn.Write([]byte("HTTP/1.0 200 OK\r\n\r\n"))
+	if err != nil {
+		http.Error(w, "write error", http.StatusInternalServerError)
+		log.Error().Err(err).Msg("")
+		return
+	}
+
+	closer := make(chan bool, 2)
+	go copyData(targetConn, clientConn, closer)
+	go copyData(clientConn, targetConn, closer)
+	<-closer
+	<-closer
+}
+
+func copyData(target, client net.Conn, closer chan bool) {
+	defer func() {
+		closer <- true
+	}()
+
+	_, err := io.Copy(target, client)
+	if err != nil {
+		log.Error().Err(err).Msg("")
+		return
+	}
+}
